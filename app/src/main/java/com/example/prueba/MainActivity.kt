aec7d00cb0e11243b8300a6916ea8c2be7660a71
Joel Sempere <joelsempere96@gmail.com>
2021-10-19 10:26:30 +0200

    Probando Kotlin, app sencilla que envia una textview a otra view

---
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.prueba
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.prueba.MainActivity.Constantes.EXTRA_NOMBRE
+
+class MainActivity : AppCompatActivity() {
+    //las constantes se declaran dentro de un objeto y con val que es inmutable
+    object Constantes{
+        const val EXTRA_NOMBRE = "MainActivity.NOMBRE"
+    }
+
+    //lateinit para asignar objetos View en onViewCreated
+    lateinit var tvMiNombre : TextView
+    lateinit var btnEnviar : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        iniciaViews()
+        btnEnviar.setOnClickListener(){
+            run{
+                val mensageAEnviar = tvMiNombre.text.toString()
+                val intent = Intent(this,RecibeTextView::class.java).apply{
+                    putExtra(EXTRA_NOMBRE,mensageAEnviar)
+                }
+                startActivity(intent)
+            }
+        }
+    }
+    fun iniciaViews(){
+        tvMiNombre=findViewById(R.id.tvMiNombre)
+        btnEnviar=findViewById(R.id.btnEnviar)
+
+    }
+}
---
+++ app/src/main/java/com/example/prueba/RecibeTextView.kt
@@ -0,0 +1,21 @@
+package com.example.prueba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.prueba.MainActivity.Constantes.EXTRA_NOMBRE
+
+class RecibeTextView : AppCompatActivity() {
+    lateinit var tvRecibeNombre : TextView;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        iniciaViews()
+        //recibe el texto de la otra view
+        tvRecibeNombre.setText(intent.getStringExtra(EXTRA_NOMBRE).toString())
+    }
+    fun iniciaViews(){
+        tvRecibeNombre= findViewById(R.id.tvRecibeNombre)
+    }
+
+}
